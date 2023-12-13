18b420fca7ebdd3213a6c63585ffc41ee09d7759
nickarakaki <nicholas.w.arakaki@gmail.com>
2023-12-13 12:37:18 -0800

    update return statement to return object rather than mapped response

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -5,7 +5,7 @@ export interface Row {
   price_def: number, 
   ratio: number,
   timestamp: Date,
-  uppper_bound: number,
+  upper_bound: number,
   lower_bound: number,
   trigger_alert: number | undefined,
 }
@@ -16,19 +16,17 @@ export class DataManipulator {
     const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
     const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
-    const upperBound = 1 + 0.1;
-    const lowerBound = 1 - 0.1;
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
     
-    return serverResponds.map((el: any) => {
-      return {
-        price_abc: priceABC,
-        price_def: priceDEF,
-        ratio,
-        timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
-        upper_bound: upperBound,
-        lower_bound: lowerBound,
-        trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined
-      };
-    })
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined
+    };
   }
 }
